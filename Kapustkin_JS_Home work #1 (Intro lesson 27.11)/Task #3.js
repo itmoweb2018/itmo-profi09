fe5470647ac60847f265cf9797a6062d4269ced4
Maxim Kapustkin <bitofmaks@gmail.com>
2018-11-29 12:30:58 +0000

    JS Lesson #1_Control task resolves

---
+++ Kapustkin_JS_Home work #1 (Intro lesson 27.11)/Task #3.js
@@ -0,0 +1,41 @@
+/*3. Дано целое число в диапазоне 0 - 9. 
+
+Вывести строку - название соответствующей цифры 
+на русском языке 
+(0 - "ноль", 1 - "один", 2 - "два", ...).*/
+
+
+<script type="text/javascript">
+      var number;
+      number = prompt();
+      if (number == 0) {
+        console.log("nil");
+      }
+      if (number == 1) {
+        console.log("one");
+      }
+      if (number == 2) {
+        console.log("two");
+      }
+      if (number == 3) {
+        console.log("three");
+      }
+      if (number == 4) {
+        console.log("four");
+      }
+      if (number == 5) {
+        console.log("five");
+      }
+      if (number == 6) {
+        console.log("six");
+      }
+      if (number == 7) {
+        console.log("seven");
+      }
+      if (number == 8) {
+        console.log("eight");
+      }
+      if (number == 9) {
+        console.log("nine");
+      }
+</script>
